7844cff4e34fdd2ca459fd1efaabcdc32b3502a4
Shailen223 <s.nanu987@gmail.com>
2023-06-29 11:56:36 +0800

    added async and await

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', getData);
 
-function getData() {
-  fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
-    .then(response => response.json())
-    .then(data => {
-      const tableBody = document.querySelector('#data-table tbody');
-      data.forEach(item => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-          <td>${item.postId}</td>
-          <td>${item.id}</td>
-          <td>${item.name}</td>
-          <td>${item.email}</td>
-          <td>${item.body}</td>
-        `;
-        tableBody.appendChild(row);
-      });
-    })
-    .catch(error => console.log(error));
+async function getData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/comments?postId=1');
+    const data = await response.json();
+    
+    const tableBody = document.querySelector('#data-table tbody');
+    data.forEach(item => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${item.postId}</td>
+        <td>${item.id}</td>
+        <td>${item.name}</td>
+        <td>${item.email}</td>
+        <td>${item.body}</td>
+      `;
+      tableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
